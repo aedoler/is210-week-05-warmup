d193f2bdd1dd8153cf46007e56cace287d07ad76
Alex Doler <aedoler@gmail.com>
2015-09-25 22:06:46 +0000

    fixed pylint errors

--- task_01/__init__.py
+++ task_01/__init__.py
@@ -1,11 +1,3 @@
 #!user/bin/env python
 # -*- coding: utf-8 -*-
 """Module that has peanut butter"""
-
-import peanut
-
-
-
-if peanut.BUTTER: print "I am a Truthy"
-
-if not peanut.OIL: print "I am a Falsy"
